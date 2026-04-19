4b2731d556a3c20abd194909d7533e50f82ac243
anthef <anthonyef09@gmail.com>
2026-04-20 00:00:10 +0700

    Update mobile-app/ios/Runner/AppDelegate.swift

--- mobile-app/ios/Runner/AppDelegate.swift
+++ mobile-app/ios/Runner/AppDelegate.swift
@@ -8,6 +8,29 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if let controller = window?.rootViewController as? FlutterViewController {
+      let channel = FlutterMethodChannel(name: "imu_node/storage", binaryMessenger: controller.binaryMessenger)
+      channel.setMethodCallHandler { call, result in
+        if call.method == "getFreeStorageMb" {
+          let freeMb = self.getFreeStorageMb()
+          result(freeMb)
+        } else {
+          result(FlutterMethodNotImplemented)
+        }
+      }
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func getFreeStorageMb() -> Int {
+    do {
+      let attrs = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory())
+      if let free = attrs[.systemFreeSize] as? NSNumber {
+        return free.intValue / (1024 * 1024)
+      }
+    } catch {
+      return -1
+    }
+    return -1
   }
 }
