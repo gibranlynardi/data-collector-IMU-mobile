5f165957c88cf098531d4aaac01d6b33df780d45
anthef <anthonyef09@gmail.com>
2026-04-20 00:00:11 +0700

    Update mobile-app/lib/main.dart

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'dart:io' show Platform;
 import 'screens/dashboard_screen.dart'; 
 
 void main() {
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: DashboardScreen(), 
