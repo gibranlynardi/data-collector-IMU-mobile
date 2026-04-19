a5e53341fddc8200d7a23d7ce1f36e4b63733c86
anthef <anthonyef09@gmail.com>
2026-04-19 14:25:27 +0700

    Add protoc-gen-dart.cmd for Dart plugin execution

---
+++ tools/protoc-gen-dart.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\antho\AppData\Local\Microsoft\WinGet\Packages\Google.DartSDK_Microsoft.Winget.Source_8wekyb3d8bbwe\dart-sdk\bin\dart.exe" pub global run protoc_plugin:protoc_plugin %*
