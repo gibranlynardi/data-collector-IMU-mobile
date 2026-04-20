b70b2258e9b837f16fbf6b9d7281b47ced3a3350
anthef <anthonyef09@gmail.com>
2026-04-20 13:37:27 +0700

    Update mobile-app/macos/Flutter/GeneratedPluginRegistrant.swift

--- mobile-app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,16 @@ import Foundation
 
 import battery_plus
 import connectivity_plus
+import package_info_plus
 import shared_preferences_foundation
 import sqflite_darwin
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
